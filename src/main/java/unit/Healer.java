2c2c3002a2a241392e03e852a697d3c45103ec04
Matias Ruiz Huidobro <ruizhuidobro.matias@gmail.com>
2019-11-06 19:03:24 -0300

    Pusheo healer?

---
+++ src/main/java/unit/Healer.java
@@ -0,0 +1,101 @@
+package unit;
+
+import cell.Cell;
+import cellState.EmptyCell;
+import cellState.OccupiedCell;
+import exceptions.MovementException;
+
+public class Healer implements Unit{
+
+    private Integer hp = 75;
+    private Integer cost = 2;
+    private Cell cell;
+    private Integer meleeDamage = 0;
+    private Integer rangedDamage = 0;
+    private Integer healingAmount = 15;
+    private String type = "Healer";
+
+    @Override
+    public void useAbility(Unit unit) {
+        if (unit.type() != "artillery") {//si no es una catapulta
+            unit.applyDamage((-1)*healingAmount);
+        }else{
+            //throw new AbilityException("Can't heal artilley units");
+        }
+    }
+
+    @Override
+    public void moveTo(Cell nextCell) {
+        Cell actualCell = this.getCell();
+
+        if (this.unitCanMove(actualCell, nextCell)){
+            // Libero la celda
+            actualCell.changeState(new EmptyCell());
+            actualCell.setUnit(null);
+
+            // Lleno la nueva celda
+            nextCell.changeState(new OccupiedCell());
+            nextCell.setUnit(this);
+            this.setCell(nextCell);
+        }
+
+    }
+
+    @Override
+    public void applyDamage(Integer damage) {
+        this.hp -= damage;
+    }
+
+    @Override
+    public void setCell(Cell cell) {
+        this.cell = cell;
+        cell.setUnit(this);
+        cell.changeState(new OccupiedCell());
+    }
+
+    @Override
+    public Cell getCell(Cell cell) {
+        return null;
+    }
+
+    @Override
+    public boolean isAlive() {
+        return false;
+    }
+
+    @Override
+    public Integer getCost() {
+        return null;
+    }
+
+    @Override
+    public Cell getCell() {
+        return cell;
+    }
+
+    @Override
+    public void die() {
+
+    }
+
+    @Override
+    public String type() {
+        return this.type;
+    }
+
+    private Boolean unitCanMove(Cell actualCell, Cell nextCell){
+
+        if(!(getDistance(actualCell, nextCell) == 1)){
+            throw new MovementException("No puedo moverme hasta ahi");
+        }
+        return true;
+    }
+
+    private Integer getDistance(Cell actualCell, Cell nextCell){
+        Integer xPosition = Math.abs(actualCell.getXPosition() - nextCell.getXPosition());
+        Integer yPosition = Math.abs(actualCell.getYPosition() - nextCell.getYPosition());
+
+        return xPosition + yPosition;
+    }
+
+}
