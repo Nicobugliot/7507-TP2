5ad6bacbbb78233b5982012a45ce4cba6169007f
Matias Ruiz Huidobro <ruizhuidobro.matias@gmail.com>
2019-11-23 11:33:35 -0300

    Agregada una nueva Excepción para errores al iniciar el juego y refactorizada la clase algo chass para que pueda manejar los turnos.

--- src/main/java/Modelo/AlgoChess.java
+++ src/main/java/Modelo/AlgoChess.java
@@ -1,24 +1,31 @@
 package Modelo;
 
+import Modelo.exceptions.GameErrorException;
+
+import java.util.Random;
+
 public class AlgoChess {
 
-    private static Player firstPlayer;
-    private static Player secondPlayer;
     private static Board board = Board.getBoard();
+    private Integer MAX_PLAYERS = 2;
+    private Player[] players = new Player[MAX_PLAYERS+1];
+    private Integer playersAdded = 0;
+    private Integer actingTeam;
+    private Boolean GAME_OVER = false;
 
     public AlgoChess() {
     }
 
-
-    public void addPlayer(Player player) {
-        if (firstPlayer == null) {
-            player.setTeam(1);
-            this.firstPlayer = player;
-        }else if(secondPlayer == null){
-            player.setTeam(2);
-            this.secondPlayer = player;
+    /*Función para agregar jugadores, permite un máximo de MAX_PLAYERS jugadores y le asigna a cada jugador su posición
+    en el arreglo como equipo, es decir el primer jugador agregado tiene el equipo 1, omite la posición 0
+     */
+    public void addPlayer(Player player) throws GameErrorException {
+        if (playersAdded == MAX_PLAYERS) throw new GameErrorException("Max amount of players reached");
+        else{
+            ++playersAdded; //así el jugador 1 esta en la posición 1
+            player.setTeam(playersAdded);
+            players[playersAdded] = player;
         }
-        //TODO si ya estan los dos jugadores entonces que no deje crear mas
     }
 
     public Cell getCell(Integer positionX, Integer positionY){
@@ -27,5 +34,16 @@ public class AlgoChess {
 
     public void setBoard(Player firstPlayer, Player secondPlayer) {
         board.setBoardCells(firstPlayer, secondPlayer);
+    }
+
+    public void startGame(){
+        Integer startingTeam = (new Random()).nextInt(2) + 1;//un número al azar de 0 a 2, le sumo uno para que represente los equipos 1 y 2
+        this.actingTeam  = startingTeam;
+        Integer nextTeam = ++startingTeam % (MAX_PLAYERS+1);
+        while(!GAME_OVER){
+            this.actingTeam  = nextTeam;
+            nextTeam = ++actingTeam % (MAX_PLAYERS+1);
+            actingTeam = (actingTeam != 0 ? actingTeam : ++actingTeam); //no hay jugador 0
+        }
     }
 }
